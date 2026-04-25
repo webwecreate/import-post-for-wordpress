75f21d3b7cf9694e5f3a4f5c62fd7b168f4e3194
webwecreate <kk.navee.11@Gmail.com>
2026-04-26 03:13:20 +0700

    update: author + author uri

--- csv-post-importer.php
+++ csv-post-importer.php
@@ -6,8 +6,8 @@
  * Version:           1.0.0
  * Requires at least: 6.0
  * Requires PHP:      8.0
- * Author:            Your Name
- * Author URI:        https://yoursite.com
+ * Author:            Webwecreate.com
+ * Author URI:        https://Webwecreate.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       csv-post-importer
